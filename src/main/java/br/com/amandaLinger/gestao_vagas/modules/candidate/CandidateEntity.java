e2e2fc944c64fd17c6b0a61729f7fcd401f765f4
Amanda Marques <amandamarqueslinger@gmail.com>
2026-06-16 16:06:29 -0300

    inserindo validações com Spring boot e lombok

--- src/main/java/br/com/amandaLinger/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/amandaLinger/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package br.com.amandaLinger.gestao_vagas.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -8,9 +11,16 @@ import java.util.UUID;
 public class CandidateEntity {
 
     private UUID id;
-    private String nome;
+    private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+" , message = "O campo [username] não deve conter espaços")
     private String username;
+
+    @Email(message="O campo [email] deve conter um e-mail válido") //faz a validação para verificar se realmente está sendo digitado um email válido
     private String email;
+
+    @Length(min=8, max=100) //aplicando regra para o tamanho da senha
+    private String password;
     private String description;
     private String curriculum;
 }
